dd6871a259084f80f170eb7543d1ad627c22135c
DamjanT1988 <Damjan_t@hotmail.com>
2024-09-21 13:53:01 +0200

    2

--- NordpoolAppDamjan/Controllers/HomeController.cs
+++ NordpoolAppDamjan/Controllers/HomeController.cs
@@ -6,27 +6,34 @@ namespace NordpoolAppDamjan.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly UMMService _ummService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(UMMService ummService)
         {
-            _logger = logger;
+            _ummService = ummService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            // Get UMM data for production unavailability
+            var umms = await _ummService.GetProductionUnavailabilityUMMsAsync(DateTime.Today);
+
+            // Group by ProductionType and calculate total unavailable capacity
+            var capacityByType = umms.GroupBy(umm => umm.ProductionType)
+                                     .Select(g => new
+                                     {
+                                         ProductionType = g.Key,
+                                         TotalCapacity = g.Sum(umm => umm.UnavailableCapacity),
+                                         Area = g.First().Area // Assuming each UMMMessage has an Area field
+                                     }).ToList();
+
+            // Convert the data to JSON in the controller
+            ViewBag.CapacityDataJson = Newtonsoft.Json.JsonConvert.SerializeObject(capacityByType);
+
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
+
+
 }
