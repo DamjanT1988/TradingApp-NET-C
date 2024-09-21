f026b779900e8217169517d40cb0209d5f65789e
DamjanT1988 <Damjan_t@hotmail.com>
2024-09-21 12:41:51 +0200

    Add project files.

---
+++ NordpoolAppDamjan/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NordpoolAppDamjan.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
